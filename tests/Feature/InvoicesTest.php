46b044b5d4f7dc253ea89c577b4a92b21c0f2aab
Caleb Porzio <calebporzio@gmail.com>
2019-06-27 09:54:20 -0400

    Add TDD version

---
+++ tests/Feature/InvoicesTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class InvoicesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_see_invoices()
+    {
+        //
+    }
+}
